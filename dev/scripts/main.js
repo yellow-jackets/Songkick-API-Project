2848583fef133304abc671f6b5c3b8c0a83037bf
evbroom <evanbroom@gmail.com>
2017-06-01 16:20:45 -0400

    resolved merge conflicts

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -38,11 +38,9 @@ const sk = {};
 sk.apiKey = 'hHSjLHKTmsfByvxU';
 sk.city = 'toronto';
 
-sk.init = function () {
-  sk.getMatchingCities();
-};
 
 // write a function to get the users location from field on submit
+sk.locationResults = ""
 
 // cities that match query
 sk.getMatchingCities = function () {
@@ -56,12 +54,53 @@ sk.getMatchingCities = function () {
       apikey: sk.apiKey
     }
   }).then(function (locationResults) {
-      locationResults = locationResults.resultsPage.results.location[0].city.displayName;
-      console.log(locationResults);
+      sk.locationResults = locationResults.resultsPage.results.location[0];
+    $('.location').html(sk.locationResults.city.displayName);
+    sk.getCityShows(sk.locationResults.metroArea.id);
   });
+};
+
+sk.getCityShows = function (id) {
+  $.ajax({
+    url: `http://api.songkick.com/api/3.0/metro_areas/${id}/calendar.json`,
+    method: 'GET',
+    dataType: 'jsonp',
+    jsonp: 'jsoncallback',
+    data: {
+      apikey: sk.apiKey
+    }
+  }).then(function (res) {
+      sk.filterListByPopularity(res)
+  });
+};
+
+
+sk.filterListByPopularity = function(concertList) {
+  let drilledToConcertArray = concertList.resultsPage.results.event
+    var concertsByPop = drilledToConcertArray.sort(function(obj1, obj2){
+      return  obj2.popularity - obj1.popularity;
+  })
+  var slicedConcerts =concertsByPop.slice(0, 20)
+  sk.putConcertsInTemplate(slicedConcerts);
+};
+
+sk.putConcertsInTemplate = function(concertList){
+  console.log(concertList)
+
 };
 // http://api.songkick.com/api/3.0/search/locations.json?query={search_query}&apikey={your_api_key}
 
+//get location id and make second songkickajax request for concerts
+//get artist names from second songkickajax store in variable and use to query musiki for images
+//funnel second ajaxsongkick and musiki results into handlebar template
+//display that in our masonry div
+
+//figure out how to make it clickable into firebase (storage)
+//figure out hot update userConcertListItems with firebase->handlebar template for the userConcertListItems
+
+sk.init = function () {
+  sk.getMatchingCities();
+};
 
 
 $(function () {
